0858ec6c93d62ac9481f111b8a0924cd5011e9a2
John Ingwersen <johningwersen11@gmail.com>
2019-11-29 15:48:41 -0600

    Admin updates

--- admin.py
+++ admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ScatterKeysXAxis, ScatterKeysYAxis
 
 # Register your models here.
+admin.site.register(ScatterKeysXAxis)
+admin.site.register(ScatterKeysYAxis)
