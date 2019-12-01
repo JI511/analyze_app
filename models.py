80af37c02e39e884f939e6ca3bbe0088e0f53e54
John Ingwersen <johningwersen11@gmail.com>
2019-11-30 21:28:43 -0600

    Added new model for teams

--- models.py
+++ models.py
@@ -23,3 +23,15 @@ class ScatterKeysYAxis(models.Model):
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
         return self.name
+
+
+class BasketballTeamNames(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+
+    # Metadata
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.name
