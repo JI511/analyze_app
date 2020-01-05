64db337f5cdb6f9093fcd17c74c972d9e3400172
John Ingwersen <johningwersen11@gmail.com>
2020-01-04 19:34:27 -0600

    Graph model object working properly for outliers and normal graph display
    Deleted other models as they shouldn't have been models in the first place. The graph object now gets created everytime the page is submitted and the values are saved into the database to be tracked over time. Still need to do something with post/redirect/get so that a refresh doesnt submit the form again.

--- admin.py
+++ admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from .models import ScatterXKey, ScatterYKey, BasketballTeamName, TrendLineChoice, GridChoice, Graph
+from .models import Graph
 
 # Register your models here.
-admin.site.register(ScatterXKey)
-admin.site.register(ScatterYKey)
-admin.site.register(BasketballTeamName)
-admin.site.register(TrendLineChoice)
-admin.site.register(GridChoice)
 admin.site.register(Graph)
--- constants.py
+++ constants.py
@@ -9,3 +9,20 @@ class Defaults(object):
     min_seconds = 0
     max_seconds = 6000
     outliers = 5
+
+
+class ScatterFilters(object):
+    x_keys = sorted(['minutes_played', 'seconds_played'])
+    y_keys = sorted(['points', 'rebounds', 'assists', 'made_field_goals', 'made_three_point_field_goals',
+                     'made_free_throws', 'offensive_rebounds', 'defensive_rebounds', 'attempted_field_goals',
+                     'attempted_three_point_field_goals', 'attempted_free_throws', 'steals', 'blocks',
+                     'turnovers', 'game_score', 'true_shooting', 'assist_turnover_ratio'])
+    teams = sorted(['Atlanta Hawks', 'Boston Celtics', 'Brooklyn Nets', 'Charlotte Hornets', 'Chicago Bulls',
+                    'Cleveland Cavaliers', 'Dallas Mavericks', 'Denver Nuggets', 'Detroit Pistons',
+                    'Golden State Warriors', 'Houston Rockets', 'Indiana Pacers', 'Los Angeles Clippers',
+                    'Los Angeles Lakers', 'Memphis Grizzlies', 'Miami Heat', 'Milwaukee Bucks',
+                    'Minnesota Timberwolves', 'New Orleans Pelicans', 'New York Knicks', 'Oklahoma City Thunder',
+                    'Orlando Magic', 'Philadelphia 76ers', 'Phoenix Suns', 'Portland Trailblazers', 'Sacramento Kings',
+                    'San Antonio Spurs', 'Toronto Raptors'])
+    grid_choices = ['Enable', 'Disable']
+    trend_choices = ['Enable', 'Disable']
