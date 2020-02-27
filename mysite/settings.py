9118261e7c0f2000be76871be3f749969e8a835c
John Ingwersen <johningwersen11@gmail.com>
2020-02-27 15:48:51 -0600

    Updated actual csv location to fix broken dates

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'analyze', 'static')
 
 # SENDFILE settings
 SENDFILE_BACKEND = 'sendfile.backends.development'
