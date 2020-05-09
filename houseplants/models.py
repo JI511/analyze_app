a2b91b78723209c893b57d4a96444cb07a0bdb8f
John Ingwersen <johningwersen11@gmail.com>
2020-05-09 16:19:14 -0500

    Start of model for houseplants

--- houseplants/models.py
+++ houseplants/models.py
@@ -1,3 +1,11 @@
+import os
 from django.db import models
 
+
 # Create your models here.
+class Graph(models.Model):
+    image_id = models.AutoField(primary_key=True)
+    image_name = models.CharField(max_length=200)
+
+    def get_media_url(self):
+        return os.path.join('Media', 'Houseplant_Images', str(self.image_name))
--- houseplants/urls.py
+++ houseplants/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'houseplants'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('reddit_images/', views.reddit_images, name='reddit_images')
 ]
