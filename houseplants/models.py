7177b8f888df88b85cc290a0c43bae5e4906d312
John Ingwersen <johningwersen11@gmail.com>
2020-05-09 16:50:15 -0500

    model updates

--- houseplants/models.py
+++ houseplants/models.py
@@ -3,9 +3,12 @@ from django.db import models
 
 
 # Create your models here.
-class Graph(models.Model):
+class HouseplantItem(models.Model):
     image_id = models.AutoField(primary_key=True)
     image_name = models.CharField(max_length=200)
 
     def get_media_url(self):
         return os.path.join('Media', 'Houseplant_Images', str(self.image_name))
+
+    def __str__(self):
+        return str(self.image_name)
