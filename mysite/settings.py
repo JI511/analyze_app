98f46f432c360568549c90d752a07bc0b4e8cdda
John Ingwersen <johningwersen11@gmail.com>
2020-05-25 17:06:32 -0500

    Updated how media directory works. Can now display images properly

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'analyze', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # SENDFILE settings
 SENDFILE_BACKEND = 'sendfile.backends.development'
