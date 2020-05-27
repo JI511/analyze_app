c27564b8c7369676f55a07378088a5cf688b8048
John Ingwersen <johningwersen11@gmail.com>
2020-05-27 10:00:03 -0500

    Added removal of model objects in populate_db script

--- houseplants/management/commands/populate_db.py
+++ houseplants/management/commands/populate_db.py
@@ -9,20 +9,37 @@ class Command(BaseCommand):
     """
     Base class for creating command.
     """
-    def _create_items(self):
+    def add_arguments(self, parser):
+        parser.add_argument('image_dir', type=str)
+        parser.add_argument(
+            '--remove',
+            action='store_true',
+            help='Delete model objects before creating/adding',
+        )
+
+    def _create_items(self, directory):
         """
         Creates houseplant model objects in a given directory.
         :return:
         """
-        directory = r'C:\Users\ingwe\Desktop\Programs\analyze_django\mysite\analyze_app\media\Houseplant_Images'
-        for plant_image in os.listdir(directory):
-            image_name = os.path.splitext(plant_image)[0]
-            model_item = HouseplantItem(image_name=image_name)
-            model_item.save()
-            print("Item named '%s' has been added." % image_name)
+        if os.path.exists(directory) and os.path.isdir(directory):
+            for plant_image in os.listdir(directory):
+                image_name = os.path.splitext(plant_image)[0]
+                model_item = HouseplantItem(image_name=image_name)
+                model_item.save()
+                print("Item named '%s' has been added." % image_name)
+
+    def _remove_items(self):
+        """
+        Removes all model objects.
+        """
+        for model_item in HouseplantItem.objects.all():
+            model_item.delete()
+            print("Removal of %s complete." % model_item)
 
     def handle(self, *args, **options):
         """
         Calls what is relevant
         """
-        self._create_items()
+        self._remove_items()
+        self._create_items(options['image_dir'])
