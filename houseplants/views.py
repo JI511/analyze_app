ec8ca468510054fc0e80ab8117acc440e6b1c214
John Ingwersen <johningwersen11@gmail.com>
2020-06-02 21:51:28 -0500

    Page list created

--- houseplants/views.py
+++ houseplants/views.py
@@ -16,9 +16,11 @@ def index(request):
 
 
 def reddit_images(request):
-    image_display_count = 5
+    image_row_display_count = 5
+    page_image_limit = 20
     obj_list = list()
-    sub_list = list()
+    page_list = list()
+    sub_image_list = list()
     selected_sort = 'Aspect Ratio (High to Low)'
     houseplant_objs = sorted(HouseplantItem.objects.all(), key=lambda obj: obj.get_aspect_ratio(), reverse=True)
 
@@ -34,18 +36,27 @@ def reddit_images(request):
 
     # currently sorting by aspect ratio, maybe also some function of image area?
     for i, item in enumerate(houseplant_objs, start=1):
-        sub_list.append(item)
-        if i % image_display_count == 0:
+        sub_image_list.append(item)
+        if i % image_row_display_count == 0:
             # reorder the sub list so everything doesnt look ordered as much
-            random.shuffle(sub_list)
-            obj_list.append(sub_list)
-            sub_list = list()
+            random.shuffle(sub_image_list)
+            obj_list.append(sub_image_list)
+            sub_image_list = list()
+    sub_page_list = list()
+    for i, item in enumerate(obj_list, start=1):
+        sub_page_list.append(item)
+        if i % (page_image_limit / image_row_display_count) == 0:
+            page_list.append(sub_page_list)
+            sub_page_list = list()
 
-    if len(sub_list) != 0:
-        obj_list.append(sub_list)
+    for a in page_list:
+        print(a)
+
+    if len(sub_image_list) != 0:
+        obj_list.append(sub_image_list)
 
     template_dict = {
-        'houseplant_images': obj_list,
+        'houseplant_images': page_list[0],
         'sorting_items': sorting_items,
         'selected_sort': selected_sort,
     }
