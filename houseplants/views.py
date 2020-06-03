6f285b60074eb825d7e05aff626f78529da74ad7
John Ingwersen <johningwersen11@gmail.com>
2020-06-02 20:57:28 -0500

    Added form linkage and dropdown display update

--- houseplants/views.py
+++ houseplants/views.py
@@ -5,21 +5,35 @@ import random
 # Create your views here.
 
 
+sorting_items = [
+    'Aspect Ratio (High to Low)',
+    'Aspect Ratio (Low to High)',
+]
+
+
 def index(request):
     return render(request, 'houseplants/index.html', {})
 
 
 def reddit_images(request):
-    if request.method == 'POST':
-        print(request.POST)
-        selected_sort = request.POST.get('sorting_method')
-        print(selected_sort)
-        
     image_display_count = 5
     obj_list = list()
     sub_list = list()
+    selected_sort = 'Aspect Ratio (High to Low)'
+    houseplant_objs = sorted(HouseplantItem.objects.all(), key=lambda obj: obj.get_aspect_ratio(), reverse=True)
+
+    if request.method == 'POST':
+        print(request.POST)
+        selected_sort = request.POST.get('sorting_method')
+        if selected_sort == sorting_items[0]:
+            houseplant_objs = sorted(HouseplantItem.objects.all(), key=lambda obj: obj.get_aspect_ratio(), reverse=True)
+            print('High to Low')
+        elif selected_sort == sorting_items[1]:
+            houseplant_objs = sorted(HouseplantItem.objects.all(), key=lambda obj: obj.get_aspect_ratio())
+            print('Low to High')
+
     # currently sorting by aspect ratio, maybe also some function of image area?
-    for i, item in enumerate(sorted(HouseplantItem.objects.all(), key=lambda obj: obj.get_aspect_ratio()), start=1):
+    for i, item in enumerate(houseplant_objs, start=1):
         sub_list.append(item)
         if i % image_display_count == 0:
             # reorder the sub list so everything doesnt look ordered as much
@@ -32,7 +46,7 @@ def reddit_images(request):
 
     template_dict = {
         'houseplant_images': obj_list,
-        'sorting_items': ['Aspect Ratio (High to Low)', 'Aspect Ratio (Low to High)'],
-        'selected_sort': 'Aspect Ratio (Low to High)',
+        'sorting_items': sorting_items,
+        'selected_sort': selected_sort,
     }
     return render(request, 'houseplants/reddit_images.html', template_dict)
