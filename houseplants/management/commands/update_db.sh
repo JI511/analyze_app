2fe81d64db119fd746c536c985d198a53837563c
John Ingwersen <johningwersen11@gmail.com>
2020-06-07 18:10:45 -0500

    Changed bash script to use arugment instead of hard coded path

--- houseplants/management/commands/update_db.sh
+++ houseplants/management/commands/update_db.sh
@@ -6,6 +6,8 @@ cd ../../../
 python manage.py remove_model_items
 python manage.py makemigrations houseplants
 python manage.py migrate houseplants
-python manage.py populate_db ~/Desktop/Programs/analyze_django/mysite/analyze_app/media/Houseplant_Images/
+# The argument is the directory where the wanted media files reside.
+python manage.py populate_db $1
 
-# will need support to activate Python virtual env
+# will need support to activate Python virtual env? All this really needs is Django support but you never know
+# what the host computer may have.
