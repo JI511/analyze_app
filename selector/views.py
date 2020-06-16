aff5d722d3175cd0028bbaed30daa7898c4b9ce1
John Ingwersen <johningwersen11@gmail.com>
2020-06-16 18:39:05 -0500

    Added for loop for column creation

--- selector/views.py
+++ selector/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def index(request):
     temp_dict = {
-        'app_names': ['analyze', 'houseplants']
+        'app_names': ['analyze', 'houseplants', 'stocks'],
     }
     return render(request, 'selector/index.html', temp_dict)
+
+
+def redirect_to_app(request):
+    """
+    redirects application to appropriate django sub-app.
+    """
+    return redirect('houseplants:index')
